b90f53f258b89eddb373fc3b0241a35cbd49dbdc
UnitaryIron <emmanuellijo670@gmail.com>
2025-05-20 14:32:26 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,9 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-auth.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Import necessary Firebase modules
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.6.0/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.0/firebase-firestore.js';
+import { getStorage } from 'https://www.gstatic.com/firebasejs/9.6.0/firebase-storage.js';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDJGVLPl7IejViBCeQlMpX7cDxLAl393gc",
   authDomain: "pawportal-f48fb.firebaseapp.com",
@@ -15,7 +14,13 @@ const firebaseConfig = {
   measurementId: "G-607EM7MPP2"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 
-export { auth, GoogleAuthProvider, signInWithPopup };
+// Initialize Firebase services
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+const storage = getStorage(app);
+
+// Export Firebase services
+export { auth, firestore, storage };
